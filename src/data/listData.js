207d93b8f01d3c5049b350ecb7a7b75273163b2f
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-09-13 21:38:17 +0530

    initial commit

---
+++ src/data/listData.js
@@ -0,0 +1,15 @@
+export const listsHeader = {
+  name: "Name",
+  admissionNo: "Admission No."
+};
+
+export const listsData = [
+  {
+    name: "aaa",
+    admissionNo: "1000"
+  },
+  {
+    name: "bbb",
+    admissionNo: "1100"
+  }
+];
